8f49f1ee84e06b4fbdcf2970a1b98dc7ddc2b96f
Ryunosuke Karimata <jpnryunosuke@gmail.com>
2019-05-21 23:57:03 +0900

    MainActivity -> chage color and layout. MenuActivity -> change navigation icon. add "talk icon" and "setting icon" add "curve"(form,button)

--- src/main/java/xyz/ryujpn/mymessenger/MenuActivity.kt
+++ src/main/java/xyz/ryujpn/mymessenger/MenuActivity.kt
@@ -2,29 +2,30 @@ package xyz.ryujpn.mymessenger
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
-import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
-import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
-    private lateinit var textMessage: TextView
 
-    private var mTabsPagerAdapter: TabsPagerAdapter? = null
+
 
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                textMessage.setText(R.string.title_home)
-
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.frameLayout, HomeFragment())
+                    .commit()
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                textMessage.setText(R.string.title_dashboard)
+            R.id.navigation_talk -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.frameLayout, TalkFragment())
+                    .commit()
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                textMessage.setText(R.string.title_notifications)
+            R.id.navigation_setting -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.frameLayout,SettingFragment())
+                    .commit()
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -34,34 +35,13 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
-        // Tab List
-        val tabsFragments= arrayListOf(
-            HomeFragment::class.java,
-            TalkFragment::class.java,
-            SettingFragment::class.java
-        )
-        // TabのAdapter
-        mTabsPagerAdapter = TabsPagerAdapter(supportFragmentManager, tabsFragments)
-
-        // ViewPagerにAdapterを設定
-        container02.adapter = mTabsPagerAdapter
-        container02.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
-            override fun onPageScrollStateChanged(p0: Int) {
-
-            }
-
-            override fun onPageScrolled(p0: Int, p1: Float, p2: Int) {
-
-            }
-
-            override fun onPageSelected(position: Int) {
-
-            }
-
-        })
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        textMessage = findViewById(R.id.message)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frameLayout, HomeFragment())
+            .commit()
+
+       navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
 }
